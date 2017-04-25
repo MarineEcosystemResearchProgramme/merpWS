05bfdb2b93ba0f4ff9756ca5c03415a45970231c
Rems <remi.vergnon@free.fr>
2017-04-25 15:20:51 +0100

    cleaned up emodnet_getallplatform.R

--- R/emodnet_getallplatform.R
+++ R/emodnet_getallplatform.R
@@ -9,25 +9,24 @@
 
 emodnet_getallplatform <- function(){
   myurl <- "http://www.emodnet-physics.eu/map/Service/WSEmodnet2.aspx?q=GetAllPlatforms&Format=text/xml"
-  inter <- getURL(myurl)
-  inter1 <- gsub(pattern = "utf-16", replacement = "utf-8", x = inter, fixed = TRUE)# use exact matching
-  # check for some illegal characters
-  if(grep(inter1, pattern = "&") == 1) inter1 <- gsub(inter1, pattern = "&", replacement = "&amp;")
-  myxml <- xmlParse(inter1)# erroneous encoding!xmlParse always used the encoding provided by the document, which is wrong in this case. so we need to change
-  xmltop = xmlRoot(myxml)
+  temp <- getURL(myurl)
+  temp <- gsub(pattern = "utf-16", replacement = "utf-8", x = temp, fixed = TRUE)
+  if(grep(temp, pattern = "&") == 1) temp <- gsub(temp, pattern = "&", replacement = "&amp;")
+  myxml <- xmlParse(temp)
+  xmltop <- xmlRoot(myxml)
   size <- xmlSize(xmltop)
-  mynames <- unique(unlist(sapply(c(1:size),function(i)names(getChildrenStrings(xmltop[[i]])))))
+  mynames <- unique(unlist(sapply(c(1:size), function(i)names(getChildrenStrings(xmltop[[i]])))))
   
-  vres <- matrix(NA,ncol= length(mynames),nrow=size)
-  nbnames <- length (mynames)
+  res <- matrix(NA, ncol = length(mynames), nrow = size)
+  nbnames <- length(mynames)
   for (i in 1:size)
   {
     othernames <- names(getChildrenStrings(xmltop[[i]]))
-    idx <- match(othernames,mynames)
-    nbnames <- c(nbnames, length (othernames))
-    vres[i,c(idx)] <- getChildrenStrings(xmltop[[i]])
+    idx <- match(othernames, mynames)
+    nbnames <- c(nbnames, length(othernames))
+    res[i, c(idx)] <- getChildrenStrings(xmltop[[i]])
   }
-  vres1 <- as.data.frame(vres)
-  names(vres1) <- mynames
-  vres1
+  res <- as.data.frame(res)
+  names(res) <- mynames
+  res
 }
