a675019121639ea22c60f5277e57d5ac37c26d0c
Rems <remi.vergnon@free.fr>
2017-04-25 15:25:57 +0100

    cleaned up emodnet_getdata_atplatform.R

--- R/emodnet_getdata_atplatform.R
+++ R/emodnet_getdata_atplatform.R
@@ -29,40 +29,40 @@
 emodnet_getdata_atplatform <- function(platformid, paramcode){
   myurl <- paste("http://www.emodnet-physics.eu/map/Service/WSEmodnet2.aspx?q=GetAllLatestDataCode&PlatformID=",
                  platformid, "&ParamCode=", paramcode, sep = "")
-  inter <- getURL(myurl)
-  inter1 <- gsub(pattern = "utf-16", replacement = "utf-8", x = inter, fixed = TRUE)# use exact matching
-  myxml <- xmlParse(inter1)
-  xmltop = xmlRoot(myxml)
+  temp <- getURL(myurl)
+  temp <- gsub(pattern = "utf-16", replacement = "utf-8", x = temp, fixed = TRUE)# use exact matching
+  myxml <- xmlParse(temp)
+  xmltop <- xmlRoot(myxml)
   size <- xmlSize(xmltop)
-  mynames <- unique(c(unlist(sapply(c(1:size),function(i)names(getChildrenStrings(xmltop[[i]]))))))
+  mynames <- unique(c(unlist(sapply(c(1:size), function(i)names(getChildrenStrings(xmltop[[i]]))))))
   
-  vres <- matrix(NA,ncol= length(mynames),nrow=size)
+  res <- matrix(NA, ncol = length(mynames), nrow = size)
   nbnames <- length (mynames)
   for (i in 1:size)
   {
     othernames <- names(getChildrenStrings(xmltop[[i]]))
-    idx <- match(othernames,mynames)
+    idx <- match(othernames, mynames)
     nbnames <- c(nbnames, length (othernames))
-    vres[i,c(idx)] <- getChildrenStrings(xmltop[[i]])
+    res[i, c(idx)] <- getChildrenStrings(xmltop[[i]])
   }
-  vres1 <- as.data.frame(vres)
-  names(vres1) <- mynames
+  res <- as.data.frame(res)
+  names(res) <- mynames
 
-  vres1$rawdata <- as.character(vres1$ParamValue) %>%
+  res$rawdata <- as.character(res$ParamValue) %>%
     strsplit(., split = ";", fixed = T) %>%
     sapply(., function(x) gsub(x[grep(x, pattern = "TEMP=")], pattern = "TEMP=", replacement = "")) %>%
     gsub(., pattern = ",", replacement = ".") %>%
     as.numeric(.)
   
   # dealing with data quality
-  vres1$data_quality <- rep(NA, nrow(vres1))
-  vres1$data_quality[grep(vres1$ParamValue, pattern = paste(paramcode, "_QC=0", sep = ""))] <- "unknown"
-  vres1$data_quality[grep(vres1$ParamValue, pattern = paste(paramcode, "_QC=1", sep = ""))] <- "good"
-  vres1$data_quality[grep(vres1$ParamValue, pattern = paste(paramcode, "_QC=2", sep = ""))] <- "probably_good"
-  vres1$data_quality[grep(vres1$ParamValue, pattern = paste(paramcode, "_QC=3", sep = ""))] <- "potentially_correctable"
-  vres1$data_quality[grep(vres1$ParamValue, pattern = paste(paramcode, "_QC=4", sep = ""))] <- "bad"
-  vres1$data_quality[grep(vres1$ParamValue, pattern = paste(paramcode, "_QC=7", sep = ""))] <- "nominal_value"
-  vres1$data_quality[grep(vres1$ParamValue, pattern = paste(paramcode, "_QC=8", sep = ""))] <- "interpolated_value"
-  vres1$data_quality[grep(vres1$ParamValue, pattern = paste(paramcode, "_QC=9", sep = ""))] <- "missing_value"
-  vres1[grep(vres1$ParamValue, pattern = paste(paramcode, "=", sep = "")), ]
+  res$data_quality <- rep(NA, nrow(res))
+  res$data_quality[grep(res$ParamValue, pattern = paste(paramcode, "_QC=0", sep = ""))] <- "unknown"
+  res$data_quality[grep(res$ParamValue, pattern = paste(paramcode, "_QC=1", sep = ""))] <- "good"
+  res$data_quality[grep(res$ParamValue, pattern = paste(paramcode, "_QC=2", sep = ""))] <- "probably_good"
+  res$data_quality[grep(res$ParamValue, pattern = paste(paramcode, "_QC=3", sep = ""))] <- "potentially_correctable"
+  res$data_quality[grep(res$ParamValue, pattern = paste(paramcode, "_QC=4", sep = ""))] <- "bad"
+  res$data_quality[grep(res$ParamValue, pattern = paste(paramcode, "_QC=7", sep = ""))] <- "nominal_value"
+  res$data_quality[grep(res$ParamValue, pattern = paste(paramcode, "_QC=8", sep = ""))] <- "interpolated_value"
+  res$data_quality[grep(res$ParamValue, pattern = paste(paramcode, "_QC=9", sep = ""))] <- "missing_value"
+  res[grep(res$ParamValue, pattern = paste(paramcode, "=", sep = "")), ]
 }
